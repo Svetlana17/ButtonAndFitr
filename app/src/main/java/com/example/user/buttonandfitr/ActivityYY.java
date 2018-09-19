2367cf8579561ad6b95fecf82c891f95ba40cf18
Users <lana.chebotareva.95@mail.ru>
2018-09-20 00:02:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/buttonandfitr/ActivityYY.java
@@ -0,0 +1,240 @@
+package com.example.user.buttonandfitr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class ActivityYY extends  AppCompatActivity implements SensorEventListener, View.OnClickListener {
+    private SensorManager mSensorManager;
+    Sensor sensorAccelerometr;
+    GraphView graph;
+    private double graph2LastYValue = 5d;
+    private double graph2LastYYValue = 5d;
+    private Double[] dataPoints;
+    LineGraphSeries<DataPoint> seriesY;
+    LineGraphSeries<DataPoint> seriesYY;
+    private Thread thread;
+    private boolean plotData = true;
+    float yy;
+    private float On_1 = 1;
+    private float altha = 0.1f;
+    private boolean state;
+    private int timer = 0;
+    Button mButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yy);
+        mButton = (Button) findViewById(R.id.buttonYY);
+        mButton.setOnClickListener((View.OnClickListener) this);
+        state = false;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorAccelerometr = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        System.out.println(sensorAccelerometr);
+        graph = (GraphView) findViewById(R.id.graph);
+        seriesY = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 0),
+
+        });
+        seriesY.setColor(Color.BLACK);
+        seriesYY = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 0),
+
+        });
+        seriesYY.setColor(Color.RED);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
+        feedMultiple();
+    }
+    public void addEntry(SensorEvent event) {
+        /*     LineGraphSeries<DataPoint> series = new LineGraphSeries<>();*/
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        System.out.println(x);
+        float y = values[1];
+        System.out.println(y);
+        float z = values[2];
+        System.out.println(z);
+        if (state) {
+            timer++;
+            if (timer % 5 == 0) {
+                System.out.println(timer);
+                // saveText(event);
+            }
+        }
+        graph2LastYValue += 1d;
+
+
+        yy = (float) (On_1 + altha * (x - On_1));
+
+
+        seriesY.appendData(new DataPoint(graph2LastYValue, y), true, 20);
+        seriesYY.appendData(new DataPoint(graph2LastYYValue, yy), true, 20);
+        graph.addSeries(seriesY);
+        graph.addSeries(seriesYY);
+
+//        if (!graficflag) {
+//            graph.removeSeries(seriesXX);
+//        } else {
+//        graph.addSeries(seriesXX);
+//        graph.addSeries(seriesX);
+
+//        }
+        //*добавление фильтра
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+//                new DataPoint(x, y),
+//        });
+//        graph.addSeries(series);
+    }
+    private void addDataPoint(double acceleration) {
+        dataPoints[499] = acceleration;
+    }
+    private void feedMultiple() {
+        if (thread != null) {
+            thread.interrupt();
+        }
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (thread != null) {
+            thread.interrupt();
+        }
+        mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (plotData) {
+            addEntry(event);
+            plotData = false;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, sensorAccelerometr, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onDestroy() {
+        mSensorManager.unregisterListener(ActivityYY.this);
+        thread.interrupt();
+        super.onDestroy();
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.Giroscope:
+//                Intent intent = new Intent();
+//                intent.setClass(this, GiroscopeActivity.class);
+//
+//                startActivity(intent);
+//                finish();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+///////
+
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        Log.d(LOG_TAG, "onPrepareOptionsMenu");
+//        menu.removeItem(R.id.acselerometr);
+//
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+////        menu.add(0, REFRESH_ID, 1, R.string.acselerometr);
+//        menu.add(0, GIROSCOPE, 2, R.string.giroscope);
+//        menu.add(0, MOVENENT,3,R.string.MOVENENT);
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+////            case R.id.acselerometr:
+////                return true;
+////            case 1:
+//////                // acselerometr
+//////                Log.d(LOG_TAG, "action: acselerometr");
+//////                return true;
+////                Intent intent = new Intent();
+////                intent.setClass(this, MainActivity.class);
+////
+////                startActivity(intent);
+////                finish();
+//            case 1:
+//                // load data
+////                Log.d(LOG_TAG, "action: load data");
+////                return true;
+////            default:
+////                return super.onOptionsItemSelected(item);
+//                Intent intents = new Intent();
+//                intents.setClass(this, AccelerometrXX.class);
+//
+//                startActivity(intents);
+//                finish();
+////            case 2:
+////                // load data
+//////                Log.d(LOG_TAG, "action: load data");
+//////                return true;
+//////            default:
+//////                return super.onOptionsItemSelected(item);
+////                Intent intentss = new Intent();
+////                intentss.setClass(this, MovenentActivity.class);
+////
+////                startActivity(intentss);
+////                finish();
+//        }
+//        return false;
+//    }
+////
+//
+}
---
+++ app/src/main/java/com/example/user/buttonandfitr/GiroscopeActivity.java
@@ -0,0 +1,299 @@
+package com.example.user.buttonandfitr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class GiroscopeActivity extends   AppCompatActivity  implements SensorEventListener, View.OnClickListener {
+private SensorManager mSensorManager;
+
+
+
+        Sensor sensoGiroscope;
+        GraphView graph;
+private double graph2LastXValue = 5d;
+private double graph2LastYValue = 5d;
+private double graph2LastZValue = 5d;
+private Double[] dataPoints;
+        LineGraphSeries<DataPoint> series;
+        LineGraphSeries<DataPoint> seriesX;
+        LineGraphSeries<DataPoint> seriesZ;
+
+private Thread thread;
+private boolean plotData = true;
+        float xx;
+        float yy;
+        float zz;
+private boolean graficflag = false;
+private float On_1 = 1;
+private float altha = 0.1f;
+private boolean state;
+private int timer = 0;
+        //    Spinner spinner;
+//    String[] acxios = {"ускорение  по х", "ускорение по y ", "ускорение по z "};
+        Button mButton;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.buttonXX);
+        mButton.setOnClickListener((View.OnClickListener) this);
+        state = false;
+//        spinner = (Spinner)findViewById(R.id.spinner);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensoGiroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+//
+
+        System.out.println(sensoGiroscope);
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+        });
+        series.setColor(Color.GREEN);
+
+        seriesX = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+
+        });
+        seriesX.setColor(Color.BLACK);
+
+        seriesZ = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+        });
+        seriesZ.setColor(Color.RED);
+
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+        });
+        series.setColor(Color.BLUE);
+
+
+        graph.addSeries(seriesX);
+        graph.addSeries(series);
+        graph.addSeries(seriesZ);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
+        feedMultiple();
+        }
+
+public void addEntry(SensorEvent event) {
+        /*     LineGraphSeries<DataPoint> series = new LineGraphSeries<>();*/
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        System.out.println(x);
+        float y = values[1];
+        System.out.println(y);
+        float z = values[2];
+        System.out.println(z);
+
+        if (state) {
+        timer++;
+        if (timer % 5 == 0) {
+        System.out.println(timer);
+        // saveText(event);
+        }
+        }
+
+
+        graph2LastXValue += 1d;
+        graph2LastYValue += 1d;
+        graph2LastZValue += 1d;
+
+
+        series.appendData(new DataPoint(graph2LastYValue, y), true, 20);
+        seriesX.appendData(new DataPoint(graph2LastXValue, x), true, 20);
+        seriesZ.appendData(new DataPoint(graph2LastZValue, z), true, 20);
+
+        graph.addSeries(series);
+        graph.addSeries(seriesX);
+        graph.addSeries(seriesZ);
+
+
+        //*добавление фильтра
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+//                new DataPoint(x, y),
+//        });
+//        graph.addSeries(series);
+        }
+
+private void addDataPoint(double acceleration) {
+        dataPoints[499] = acceleration;
+        }
+
+private void feedMultiple() {
+
+        if (thread != null) {
+        thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+@Override
+public void run() {
+        while (true) {
+        plotData = true;
+        try {
+        Thread.sleep(10);
+        } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        }
+        }
+        }
+        });
+
+        thread.start();
+        }
+
+
+@Override
+protected void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+        thread.interrupt();
+        }
+        mSensorManager.unregisterListener(this);
+
+        }
+
+@Override
+protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, sensoGiroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+@Override
+protected void onDestroy() {
+        mSensorManager.unregisterListener(GiroscopeActivity.this);
+        thread.interrupt();
+        super.onDestroy();
+        }
+
+public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setClass(this, AccelerometrXX.class);
+
+        startActivity(intent);
+        finish();
+        }
+public void onClicks(View v) {
+        Intent intent = new Intent();
+        intent.setClass(this, ActivityYY.class);
+
+        startActivity(intent);
+        finish();
+        }
+public void onClickss(View v) {
+        Intent intent = new Intent();
+        intent.setClass(this, ActivityZZ.class);
+
+        startActivity(intent);
+        finish();
+        }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.Giroscope:
+//                Intent intent = new Intent();
+//                intent.setClass(this, GiroscopeActivity.class);
+//
+//                startActivity(intent);
+//                finish();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+///////
+
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        Log.d(LOG_TAG, "onPrepareOptionsMenu");
+//        menu.removeItem(R.id.acselerometr);
+//
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+////        menu.add(0, REFRESH_ID, 1, R.string.acselerometr);
+//        menu.add(0, GIROSCOPE, 2, R.string.giroscope);
+//        menu.add(0, MOVENENT,3,R.string.MOVENENT);
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+////            case R.id.acselerometr:
+////                return true;
+////            case 1:
+//////                // acselerometr
+//////                Log.d(LOG_TAG, "action: acselerometr");
+//////                return true;
+////                Intent intent = new Intent();
+////                intent.setClass(this, MainActivity.class);
+////
+////                startActivity(intent);
+////                finish();
+//            case 1:
+//                // load data
+////                Log.d(LOG_TAG, "action: load data");
+////                return true;
+////            default:
+////                return super.onOptionsItemSelected(item);
+//                Intent intents = new Intent();
+//                intents.setClass(this, AccelerometrXX.class);
+//
+//                startActivity(intents);
+//                finish();
+////            case 2:
+////                // load data
+//////                Log.d(LOG_TAG, "action: load data");
+//////                return true;
+//////            default:
+//////                return super.onOptionsItemSelected(item);
+////                Intent intentss = new Intent();
+////                intentss.setClass(this, MovenentActivity.class);
+////
+////                startActivity(intentss);
+////                finish();
+//        }
+//        return false;
+//    }
+////
+//
+        }
